510859a7e8727b69fafd3eacd8f9cd2ec45523e1
Phil Phoenix <phillip.matthew.phoenix@gmail.com>
2014-09-03 12:00:44 -0600

    Added product table and model

---
+++ db/migrate/20140903175509_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+    	t.string :title
+    	t.string :subtitle
+    	t.string :author
+    	t.decimal :price
+    	t.string :sku
+    	t.text :description
+      t.timestamps
+    end
+  end
+end
